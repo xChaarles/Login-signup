a14c7322c50f7495715756501067acd7e599781c
xchaarles <carlosbp2608@gmail.com>
2024-09-17 20:33:25 -0500

    se agrega el setRole al metodo login para el rol q pertenece

--- src/main/java/com/login_registro/service/UsersManagementService.java
+++ src/main/java/com/login_registro/service/UsersManagementService.java
@@ -67,6 +67,7 @@ public class UsersManagementService {
             var refreshToken = jwtUtils.generateRefreshToken(new HashMap<>(), user);
             response.setStatusCode(200);
             response.setToken(jwt);
+            response.setRole(user.getRole());
             response.setRefreshToken(refreshToken);
             response.setExpirationTime("24Hrs");
             response.setMessage("Inicio de sesion existoso");
